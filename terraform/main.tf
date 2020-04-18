f1e4ecf89e5deb5eae69956f42065fc4134198d3
McKade <mckades@gmail.com>
2020-04-17 23:47:51 -0800

    Moved into the terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws  = "~> 2.46.0"
+    null = "~> 2.1.0"
+  }
+}
+
+provider "aws" {
+
+}
+
+resource "aws_vpc" "cs321-vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "cs321-vpc"
+  }
+}
+
+
+resource "aws_security_group" "cs371-security-group" {
+  name = "cs371-security-group"
+  vpc_id = aws_vpc.cs321-vpc.id
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]  // TODO: Update
+  }
+}
