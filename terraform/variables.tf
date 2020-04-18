d07f213d84d9c0849fcab8395f2de8b92eda3167
McKade <mckades@gmail.com>
2020-04-17 23:48:54 -0800

    First commit for variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "ami" {
+  description = "AMI for EC2"
+  default = {
+    ami = "ami-0323c3dd2da7fb37d" // Amazon Linux 2
+  }
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the entire VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR: Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR: Private Subnet"
+    default = "10.0.1.0/24"
+}
