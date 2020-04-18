de0a70b07dc23258b711a2758df53721124a83bc
McKade <mckades@gmail.com>
2020-04-17 21:15:07 -0800

    added code to get aws information

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if (( $# != 1 )); then
+    echo "Usage: source env.sh aws_profile_name "
+else
+    export AWS_PROFILE=$1
+    export AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id --profile $AWS_PROFILE`
+    export AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key --profile $AWS_PROFILE`
+    export AWS_REGION=`aws configure get region --profile $AWS_PROFILE`
+
+fi
